b52de3fd5d12a501c8632213210141149f46fc81
Lucas Collins <Lucas.Collins@LCEcorp.com>
2019-07-30 09:30:47 -0400

    Initial Commit

---
+++ NewHostApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewHostApp
+//
+//  Created by Lucas Collins on 7/30/19.
+//  Copyright © 2019 Lucas Collins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
